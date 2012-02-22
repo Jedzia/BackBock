71f1e159927dc4796bfe773849295b2cee628977
Jedzia <jed69@gmx.de>
2012-02-22 08:10:10 +0100

    Added WPG Lib

---
+++ WPG/WPG/Themes/TypeEditors/CollectionEditorControl.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ICSharpCode.WpfDesign.Designer.PropertyGrid.Editors.BrushEditor;
+using WPG.Data;
+using WPG.TypeEditors;
+
+namespace WPG.Themes.TypeEditors
+{
+    public partial class CollectionEditorControl : UserControl
+    {
+
+
+        public Property MyProperty
+        {
+            get { return (Property)GetValue(MyPropertyProperty); }
+            set { SetValue(MyPropertyProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MyPropertyProperty =
+            DependencyProperty.Register("MyProperty", typeof(Property), typeof(CollectionEditorControl), new UIPropertyMetadata(null));
+
+        
+        public Type NumerableType
+        {
+            get { return (Type)GetValue(NumerableTypeProperty); }
+            set { SetValue(NumerableTypeProperty, value); }
+        }
+
+        public static readonly DependencyProperty NumerableTypeProperty =
+            DependencyProperty.Register("NumerableType", typeof(Type), typeof(CollectionEditorControl), new UIPropertyMetadata(null));
+
+        public IEnumerable NumerableValue
+        {
+            get { return (IEnumerable)GetValue(NumerableValueProperty); }
+            set { SetValue(NumerableValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty NumerableValueProperty =
+            DependencyProperty.Register("NumerableValue", typeof(IEnumerable), typeof(CollectionEditorControl), new UIPropertyMetadata(null));
+
+        public string NumerableTypeIdentifier
+        {
+            get { return (string)GetValue(NumerableTypeIdentifierProperty); }
+            set { SetValue(NumerableTypeIdentifierProperty, value); }
+        }
+
+        public static readonly DependencyProperty NumerableTypeIdentifierProperty =
+            DependencyProperty.Register("NumerableTypeIdentifier", typeof(string), typeof(CollectionEditorControl), new UIPropertyMetadata(null));
+
+        public CollectionEditorControl()
+        {
+            InitializeComponent();            
+            //txtTypeName.Text = NumerableType.GetType().ToString();
+        }
+
+        public ICommand AddTypeCommand
+        {
+            get { return (ICommand)GetValue(AddTypeCommandProperty); }
+            set { SetValue(AddTypeCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty AddTypeCommandProperty =
+            DependencyProperty.Register("AddTypeCommand", typeof(ICommand), typeof(CollectionEditorControl), new UIPropertyMetadata(null));
+
+        public ICommand RemoveTypeCommand
+        {
+            get { return (ICommand)GetValue(RemoveTypeCommandProperty); }
+            set { SetValue(RemoveTypeCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty RemoveTypeCommandProperty =
+            DependencyProperty.Register("RemoveTypeCommand", typeof(ICommand), typeof(CollectionEditorControl), new UIPropertyMetadata(null));
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CollectionEditorWindow collEdt = new CollectionEditorWindow(this);
+            collEdt.ShowDialog();
+        }
+
+        
+    }
+}
---
+++ WPG/WPG/TypeEditors/CollectionEditorWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Windows;
+using WPG.Themes.TypeEditors;
+using System.ComponentModel;
+
+namespace WPG.TypeEditors
+{
+    /// <summary>
+    /// Interaction logic for CollectionEditor.xaml
+    /// </summary>
+    public partial class CollectionEditorWindow : Window
+    {
+        public CollectionEditorControl baseControl { get; set; }
+
+        public bool HasDefaultConstructor(Type type)
+        {
+            if (type.IsValueType)
+                return true;
+
+            var constructor = type.GetConstructor(Type.EmptyTypes);
+
+            if (constructor == null)
+                return false;
+
+            return true;
+        }
+        public CollectionEditorWindow(CollectionEditorControl ctrl)
+        {
+            InitializeComponent();
+            baseControl = ctrl;
+
+            foreach (var tmp in baseControl.NumerableValue)
+            {
+                myLst.Items.Add(tmp);
+            }
+
+            //Visibilty of cmdAdd
+
+            if (baseControl.MyProperty == null)
+            {
+                PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(baseControl.DataContext);
+                baseControl.MyProperty = new WPG.Data.Property(
+                    baseControl.DataContext, properties[baseControl.NumerableTypeIdentifier]);
+
+                //cmdAdd.Visibility = Visibility.Collapsed;
+                //cmdRemove.Visibility = Visibility.Collapsed;
+                //return;
+            }
+
+            //var aa = baseControl.MyProperty.PropertyType.GetGenericArguments();
+            if (!HasDefaultConstructor(baseControl.MyProperty.PropertyType.GetGenericArguments()[0]) || baseControl.MyProperty.IsReadOnly)
+            {
+                cmdAdd.Visibility = Visibility.Collapsed;
+            }
+
+            if (baseControl.MyProperty.IsReadOnly)
+                cmdRemove.Visibility = Visibility.Collapsed;
+
+
+            //myLst.ItemsSource = baseControl.NumerableValue;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void cmdRemove_Click(object sender, RoutedEventArgs e)
+        {
+            if (myLst.SelectedItem != null)
+            {
+                var rem = myLst.SelectedItem;
+                myLst.Items.Remove(myLst.SelectedItem);
+                if (baseControl.RemoveTypeCommand != null)
+                {
+                    baseControl.RemoveTypeCommand.Execute(rem);
+                }
+            }
+            myGrid.Instance = null;
+        }
+
+        private void myLst_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            myGrid.Instance = myLst.SelectedItem;
+        }
+
+        private void cmdOk_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void cmdAdd_Click(object sender, RoutedEventArgs e)
+        {
+            object newElem = System.Activator.CreateInstance(baseControl.MyProperty.PropertyType.GetGenericArguments()[0]);
+            if (baseControl.AddTypeCommand != null)
+            {
+                baseControl.AddTypeCommand.Execute(newElem);
+            }
+            myLst.Items.Add(newElem);
+        }
+
+
+
+
+    }
+}
