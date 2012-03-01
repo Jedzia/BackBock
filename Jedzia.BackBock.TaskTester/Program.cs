63b61a08a39e7a182766c5a2643edcd951f8937d
Jedzia <jed69@gmx.de>
2012-03-01 06:52:46 +0100

    TDD: TaskTester + Jedzia.BackBock.Tasks.Tests project

---
+++ Jedzia.BackBock.TaskTester/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Jedzia.BackBock.Tasks;
+
+namespace Jedzia.BackBock.TaskTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = TaskRegistry.GetInstance();
+            var task = service["Backup"];
+        }
+    }
+}
