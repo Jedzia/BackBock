4c30521a3e97d65ed87014be8d88c15b0f06c258
Jedzia <jed69@gmx.de>
2012-03-06 10:48:59 +0100

    Testing

---
+++ Jedzia.BackBock.TaskTester/Logger.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Framework;
+
+namespace Jedzia.BackBock.TaskTester
+{
+    class Messenger
+    {
+        public void Send(object sender, BuildErrorEventArgs e)
+        {
+            var message = string.Format("ERROR {0}:[{1}-{2}] {3}", e.Timestamp, e.ThreadId, e.SenderName, e.Message);
+            Console.WriteLine(message);
+        }
+
+        internal void Send(object sender, BuildMessageEventArgs e)
+        {
+            var message = string.Format("{0}:[{1}-{2}] {3}", e.Timestamp, e.ThreadId, e.SenderName, e.Message);
+            Console.WriteLine(message);
+        }
+    }
+
+    class Logger : ILogger
+    {
+        #region ILogger Members
+        /// <summary>
+        /// Gets or sets 
+        /// </summary>
+        public bool Enabled
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Logger"/> class.
+        /// </summary>
+        public Logger()
+        {
+            this.Enabled = true;
+        }
+
+        private Messenger messengerInstance;
+        public Messenger MessengerInstance
+        {
+            get
+            {
+                if (messengerInstance == null)
+                {
+                    this.messengerInstance = new Messenger();
+                }
+                return this.messengerInstance;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the specified event source.
+        /// </summary>
+        /// <param name="eventSource">The event source.</param>
+        public void Initialize(IEventSource eventSource)
+        {
+            eventSource.MessageRaised += Log;
+            eventSource.ErrorRaised += Log;
+        }
+
+        private void Log(object sender, BuildErrorEventArgs e)
+        {
+            if (this.Enabled)
+                MessengerInstance.Send(sender, e);
+        }
+
+        public void Log(object sender, BuildMessageEventArgs buildMessageEventArgs)
+        {
+            if (this.Enabled)
+                MessengerInstance.Send(sender, buildMessageEventArgs);
+        }
+
+        public void Shutdown()
+        {
+        }
+
+        public string Parameters
+        {
+            get
+            {
+                return string.Empty;
+            }
+            set
+            {
+            }
+        }
+
+        public LoggerVerbosity Verbosity
+        {
+            get
+            {
+                return LoggerVerbosity.Detailed;
+            }
+            set
+            {
+            }
+        }
+
+        #endregion
+    }
+}
--- Jedzia.BackBock.TaskTester/Program.cs
+++ Jedzia.BackBock.TaskTester/Program.cs
@@ -7,7 +7,17 @@ using Jedzia.BackBock.Tasks;
 namespace Jedzia.BackBock.TaskTester
 {
     using System.Collections;
+    using Microsoft.Build.Utilities;
+    using Microsoft.Build.BuildEngine;
+    using Microsoft.Build.Framework;
 
+    /// <summary>
+    /// Summary
+    /// </summary>
+    public static class Consts
+    {
+        public const string InputFileA = @"C:\Temp\FolderA";
+    }
     /// <summary>
     /// Blah
     /// </summary>
@@ -19,6 +29,58 @@ namespace Jedzia.BackBock.TaskTester
     public class Program
     {
         static void Main(string[] args)
+        {
+            //CheckBackupTask();
+            /*var task = new Zip();
+            task.TaskAction = "Create";
+            task.CompressFiles = new[] { new TaskItem(Consts.InputFileA), };
+            task.ZipFileName = new TaskItem(@"D:\TestZip.zip");
+            TestB(task);*/
+
+            var paths = new[] { Consts.InputFileA };
+            //TestA(typeof(Backup), "SourceFiles", paths);
+            TestA(typeof(Zip), "CompressFiles", paths);
+        }
+
+        private static void TestB(ITask task)
+        {
+        }
+
+        private static void TestA(Type taskType, string sourceParameter, IEnumerable<string> paths)
+        {
+            var engine = new Engine(@"D:\E\Projects\CSharp\BackBock\Jedzia.BackBock.Application\bin\Debug");
+            engine.RegisterLogger(new Logger());
+
+            var proj = engine.CreateNewProject();
+            proj.DefaultToolsVersion = "3.5";
+            var target = proj.Targets.AddNewTarget("mainTarget");
+
+            #region itemgroup
+            var big = proj.AddNewItemGroup();
+
+            var inclEle = new StringBuilder();
+            foreach (var path in paths)
+            {
+                inclEle.Append(path);
+                inclEle.Append(";");
+            }
+            inclEle.Remove(inclEle.Length, 1);
+            var include = inclEle.ToString();
+
+            BuildItem cr = big.AddNewItem("FilesToZip", include);
+            // cr.Exclude = "*.txt";
+            #endregion
+
+            //Type btasktype = typeof(Zip);
+            proj.AddNewUsingTaskFromAssemblyName(taskType.FullName, taskType.Assembly.FullName);
+            var batask = target.AddNewTask(taskType.FullName);
+            //batask.SetParameterValue("SourceFiles", @"C:\Temp\company.xmi");
+            batask.SetParameterValue(sourceParameter, @"@(FilesToZip)");
+            var pars = batask.GetParameterNames();
+
+        }
+
+        private static void CheckBackupTask()
         {
             var service = TaskRegistry.GetInstance();
             var task = (Backup)service["Backup"];
