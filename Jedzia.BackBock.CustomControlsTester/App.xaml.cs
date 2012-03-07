52e64dfb7c18e8a695a708ca6c64c783c7c5d97d
Jedzia <jed69@gmx.de>
2012-03-07 17:24:53 +0100

    Custom Controls Tester

---
+++ Jedzia.BackBock.CustomControlsTester/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Jedzia.BackBock.CustomControlsTester
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
